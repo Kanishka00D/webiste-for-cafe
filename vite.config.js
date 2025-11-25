41e51ca46d27bc1de3b6fa0fe7ee89b71ce0fc82
Kanishka Dahampath <148880966+Kanishka00D@users.noreply.github.com>
2025-11-25 15:14:41 +0530

    Update website

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/webiste-for-cafe//',
+  base: '/webiste-for-cafe',
 });
